efc3326784d72b92eefab9b21e22bdccce1e1865
COMP Omega <compomega@tutanota.com>
2016-09-17 08:16:35 -0600

    [ADD] External project for libuv.

--- vsbuild_x64.bat
+++ vsbuild_x64.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir build64 > nul 2> nul
 cd build64
-cmake -G"Visual Studio 14 2015 Win64" -DLIBUV_INCLUDE_DIR="%~dp0\deps\win64\include" -DLIBUV_LIBRARY="%~dp0\deps\win64\lib\libuv.lib" -DOPENSSL_ROOT_DIR="%~dp0\deps\win64" ..
+cmake -G"Visual Studio 14 2015 Win64" -DOPENSSL_ROOT_DIR="%~dp0\deps\win64" ..
 pause

977c024e12fe80226e8919a8530a95fbaa3c3383
COMP Omega <compomega@tutanota.com>
2016-09-17 16:38:29 -0600

    [FIX] VS20015 now builds x64 debug with only warnings in the logger and no external deps.

--- vsbuild_x64.bat
+++ vsbuild_x64.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir build64 > nul 2> nul
 cd build64
-cmake -G"Visual Studio 14 2015 Win64" -DOPENSSL_ROOT_DIR="%~dp0\deps\win64" ..
+cmake -G"Visual Studio 14 2015 Win64" ..
 pause
--- vsbuild_x86.bat
+++ vsbuild_x86.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir build32 > nul 2> nul
 cd build32
-cmake -G"Visual Studio 14 2015" -DOPENSSL_ROOT_DIR="%~dp0\deps\win32" ..
+cmake -G"Visual Studio 14 2015" ..
 pause

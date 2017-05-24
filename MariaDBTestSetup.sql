7e3df483b7987640722123902be09af6b730b88c
Omega <compomega@tutanota.com>
2017-05-24 17:50:20 -0600

    Fixed DROP USER

--- MariaDBTestSetup.sql
+++ MariaDBTestSetup.sql
@@ -1,5 +1,7 @@
 USE mysql;
-DROP USER IF EXISTS 'testuser'@'localhost';
+# See: https://stackoverflow.com/questions/598190/mysql-check-if-the-user-exists-and-drop-it
+GRANT USAGE ON *.* TO 'testuser'@'localhost';
+DROP USER 'testuser'@'localhost';
 CREATE USER 'testuser'@'localhost' IDENTIFIED BY 'un1tt3st';
 GRANT ALL PRIVILEGES ON *.* TO 'testuser'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;

0eef5bafd6e5530752622fc66000f85caded0b24
Omega <compomega@tutanota.com>
2017-05-24 18:09:25 -0600

    Try just assuming the user does not exist.

--- MariaDBTestSetup.sql
+++ MariaDBTestSetup.sql
@@ -1,7 +1,4 @@
 USE mysql;
-# See: https://stackoverflow.com/questions/598190/mysql-check-if-the-user-exists-and-drop-it
-GRANT USAGE ON *.* TO 'testuser'@'localhost';
-DROP USER 'testuser'@'localhost';
 CREATE USER 'testuser'@'localhost' IDENTIFIED BY 'un1tt3st';
 GRANT ALL PRIVILEGES ON *.* TO 'testuser'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;

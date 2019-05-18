c746e09e6575335aa0454d03138043030f7a0fc8
COMP Omega <compomega@tutanota.com>
2019-05-18 12:13:52 -0600

    [MOD] Use curl instead.

--- ci/windows-warmcache.sh
+++ ci/windows-warmcache.sh
@@ -8,12 +8,12 @@ cd $CACHE_DIR
 
 # External dependencies for Windows
 if [ ! -f external-0.1.1-${PLATFORM}.zip ]; then
-    wget -q https://github.com/comphack/external/releases/download/external-25/external-0.1.1-${PLATFORM}.zip
+    curl -L https://github.com/comphack/external/releases/download/external-25/external-0.1.1-${PLATFORM}.zip
 fi
 
 # For a re-download of the libcomp builds
 rm -f libcomp-*-${PLATFORM}.tar.bz2
-wget -q https://github.com/comphack/libcomp/releases/download/v4.1.2/libcomp-4.1.2-${PLATFORM}.zip
+curl -L https://github.com/comphack/libcomp/releases/download/v4.1.2/libcomp-4.1.2-${PLATFORM}.zip
 
 # Qt 5.12.3
 if [ ! -f Qt-5.12.3-${PLATFORM}.tar ]; then

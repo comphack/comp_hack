e62ecf1e922d1a64dd7dde27df2cbb1e3d153990
COMP Omega <compomega@tutanota.com>
2019-05-18 12:19:16 -0600

    [FIX] Fix the cache script?

--- ci/windows-warmcache.sh
+++ ci/windows-warmcache.sh
@@ -7,15 +7,21 @@ export CACHE_DIR=`pwd`/cache
 cd $CACHE_DIR
 
 # External dependencies for Windows
+echo Downloading the external dependencies
 if [ ! -f external-0.1.1-${PLATFORM}.zip ]; then
     curl -L https://github.com/comphack/external/releases/download/external-25/external-0.1.1-${PLATFORM}.zip
 fi
 
 # For a re-download of the libcomp builds
-rm -f libcomp-*-${PLATFORM}.tar.bz2
+echo Removing cached libcomp
+if [ ! -f libcomp-4.1.2-${PLATFORM}.zip ]; then
+    rm libcomp-4.1.2-${PLATFORM}.zip
+fi
+echo Downloading the external dependencies
 curl -L https://github.com/comphack/libcomp/releases/download/v4.1.2/libcomp-4.1.2-${PLATFORM}.zip
 
 # Qt 5.12.3
+echo Downloading Qt
 if [ ! -f Qt-5.12.3-${PLATFORM}.tar ]; then
     cd $ROOT_DIR
 

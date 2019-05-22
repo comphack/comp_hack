b75a7ffa3622f5d98a46394a04308569943062b3
COMP Omega <compomega@tutanota.com>
2019-05-22 06:48:10 -0600

    [ADD] Deploy script to try out.

---
+++ ci/linux-deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Load the global settings.
+source "ci/global.sh"
+
+#
+# Prepare build results to BinTray
+#
+
+echo "Patching bintray.json"
+cat "${ROOT_DIR}/ci/bintray.json.in" | sed \
+    -e "s/@TRAVIS_BRANCH@/$TRAVIS_BRANCH/g" \
+    -e "s/@TRAVIS_COMMIT@/$TRAVIS_COMMIT/g" > "${ROOT_DIR}/ci/bintray.json"
+
+echo "Copying files to deploy"
+mkdir deploy
+
+# Copy the Windows 64-bit build into the deploy folder.
+cp ${CACHE_DIR}/build-win64/* deploy/

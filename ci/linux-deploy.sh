b836ffc654404dc04c6129d64af34176c8f51473
Omega <compomega@tutanota.com>
2019-05-22 11:41:10 -0600

    Updated the location of the deploy files.

--- ci/linux-deploy.sh
+++ ci/linux-deploy.sh
@@ -13,8 +13,10 @@ cat "${ROOT_DIR}/ci/bintray.json.in" | sed \
     -e "s/@TRAVIS_BRANCH@/$TRAVIS_BRANCH/g" \
     -e "s/@TRAVIS_COMMIT@/$TRAVIS_COMMIT/g" > "${ROOT_DIR}/ci/bintray.json"
 
+echo "Contents of the cache directory:"
+ls -lh ${CACHE_DIR}
 echo "Copying files to deploy"
 mkdir deploy
 
 # Copy the Windows 64-bit build into the deploy folder.
-cp ${CACHE_DIR}/build-win64/* deploy/
+cp ${CACHE_DIR}/comp_hack-*.{zip,msi} deploy/

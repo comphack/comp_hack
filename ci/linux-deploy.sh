e676672324dc91aacf2b25da7d5c4a6822cdb064
COMP Omega <compomega@tutanota.com>
2019-05-23 06:58:13 -0600

    [MOD] Try a deploy to Dropbox.

--- ci/linux-deploy.sh
+++ ci/linux-deploy.sh
@@ -10,11 +10,13 @@ source "ci/global.sh"
 
 echo "Patching bintray.json"
 cat "${ROOT_DIR}/ci/bintray.json.in" | sed \
+    -e "s/@TRAVIS_BUILD_NUMBER@/$TRAVIS_BUILD_NUMBER/g" \
     -e "s/@TRAVIS_BRANCH@/$TRAVIS_BRANCH/g" \
     -e "s/@TRAVIS_COMMIT@/$TRAVIS_COMMIT/g" > "${ROOT_DIR}/ci/bintray.json"
 
 echo "Contents of the cache directory:"
 ls -lh ${CACHE_DIR}
+
 echo "Copying files to deploy"
 mkdir deploy
 

9b3a393d77a636e3b8c5aafedc474a9c4b8b37e9
COMP Omega <compomega@tutanota.com>
2020-09-15 18:48:56 -0600

    [MOD] Update clang format script.

--- clang_format.sh
+++ clang_format.sh
@@ -3,4 +3,4 @@ while read f
 do
 	echo $f
 	clang-format -style=file -i $f
-done < <(find -type d \( -path ./build -o -path ./deps -o -path ./libcomp/libobjgen/res \) -prune -o \( -name '*.h' -o -name '*.cpp' \) ! -name 'LookupTable*.h' -print)
+done < <(find -type d \( -path ./build -o -path ./deps -o -path ./libcomp/deps -o -path ./libcomp/libobjgen/res \) -prune -o \( -name '*.h' -o -name '*.cpp' \) ! -name 'LookupTable*.h' ! -name 'rbtree.h' -print)

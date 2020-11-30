94808e391c1fd03405166bb722098c336def97d5
COMP Omega <compomega@tutanota.com>
2020-11-30 11:53:57 -0700

    [REVERT] Problem with Invoke-WebRequest may be due to extra quotes on env variables.

--- ci/windows-updater-build.ps1
+++ ci/windows-updater-build.ps1
@@ -8,7 +8,7 @@ Write-Output "Configuration = ${env:CONFIGURATION}"
 Write-Output "Generator     = ${env:GENERATOR}"
 
 Write-Output "Installing external dependencies"
-Write-Output Invoke-WebRequest "https://github.com/comphack/external/releases/download/${env:EXTERNAL_RELEASE}/external-${env:PLATFORM}-${env:COMPILER}.zip" -OutFile "external-${env:PLATFORM}-${env:COMPILER}.zip"
+Invoke-WebRequest "https://github.com/comphack/external/releases/download/${env:EXTERNAL_RELEASE}/external-${env:PLATFORM}-${env:COMPILER}.zip" -OutFile "external-${env:PLATFORM}-${env:COMPILER}.zip"
 7z x "external-${env:PLATFORM}-${env:COMPILER}.zip"
 Remove-Item "external-${env:PLATFORM}-${env:COMPILER}.zip"
 Move-Item external* binaries
